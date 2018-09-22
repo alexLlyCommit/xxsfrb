6d8a7678be20f97349ed9d01a164e10c58c1e80c
alexLlyCommit <luolinyuanLly@163.com>
2018-09-22 10:57:41 +0800

    无状态组件

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,22 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Header extends Component {
-  state = {
-    content: "123"
-  };
-  render() {
-    const title = "这是一个标题";
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-1 col-xs-offset-11">
-            <h1>Header</h1>
-            <p>{title}</p>
-            <p>{this.state.content}</p>
-            {true ? "Hello" : "no Ok"}
-          </div>
+// 无状态组件
+const Header = props => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-xs-1 col-xs-offset-11">
+          <h1>Header</h1>
+          {true ? "Hello" : "no Ok"}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+export default Header;
