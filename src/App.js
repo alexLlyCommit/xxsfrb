b90ff8bc32f296b9cf7543c5abadf0c74a748b63
alexLlyCommit <luolinyuanLly@163.com>
2018-09-22 09:37:35 +0800

    多个组件

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
 
 class App extends Component {
   render() {
@@ -7,7 +9,17 @@ class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col-xs-1 col-xs-offset-11">
+            <Header />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
             <h1>Hello !!</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <Home />
           </div>
         </div>
       </div>
