4ef82025c93b06c4c68ecf00fa81bf0d091e1844
alexLlyCommit <luolinyuanLly@163.com>
2018-09-22 09:48:56 +0800

    输出动态数据

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 
 export default class Header extends Component {
+  state = {
+    content: "123"
+  };
   render() {
+    const title = "这是一个标题";
     return (
       <div className="container">
         <div className="row">
           <div className="col-xs-1 col-xs-offset-11">
             <h1>Header</h1>
+            <p>{title}</p>
+            <p>{this.state.content}</p>
+            {true ? "Hello" : "no Ok"}
           </div>
         </div>
       </div>
